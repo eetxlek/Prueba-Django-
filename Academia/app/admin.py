a2b58b0f9ea731b4cc94db24cc9974b1386d8475
eetxlek <eetxab@gmail.com>
2025-09-05 18:26:01 +0200

    panel admin y superuser

--- Academia/app/admin.py
+++ Academia/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Estudiante
+from .models import Curso
+from .models import Matricula   
+
+admin.site.register(Estudiante)
+admin.site.register(Curso)
+admin.site.register(Matricula)

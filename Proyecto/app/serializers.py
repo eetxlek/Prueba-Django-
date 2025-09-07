98afdc4fa18011218bc0a6df86b0602d7f7622f5
Borja Etxaburu <betxaburu@gmail.com>
2025-09-07 18:41:27 +0200

    fix

--- Proyecto/app/serializers.py
+++ Proyecto/app/serializers.py
@@ -19,20 +19,20 @@ class MatriculaSerializer(serializers.ModelSerializer):
         read_only_fields = ['fecha_matricula']  # protege fecha, que no sea modificable con POST o PUT. No se va a incluir en validated_data aunque venga en request.data.
         #ignora el campo matricula si el user lo envia. Usa el auto_now_add=True en vez del enviado ha hacer serializer.save.
 
-        # sobreescritura de validate de DRF o hook/gancho que forma parte del ciclo de vida del serializer. 
-        # Permite validacion personalizada sobre objetos (varios campos a la vez)
-        # integra el clean al flujo validacion de DRF
-        def validate(self, data):   #drf devuelve un 400 Bad request mediante el def clean.
-            # Crear una instancia temporal con los datos recibidos en DRF serializer.is_valid(campo o objeto) que llama internamente a este validate.
-            instance = Matricula(**data)
-            #esta instancia no tiene .pk , django trata como nueva instancia. En clean al filtrar matricula puede fallar actualizacion. 
-            # pk es necesario para diferenciar crear de actualizar en el clean. Conservamos antes de pasar al clean. Evita FALSOS POSITIVOS. 
-            if self.instance:
-                instance.pk = self.instance.pk
-            try:
-                instance.clean()  # Ejecuta reglas de negocio definidas en el modelo
-            except DjangoValidationError as e:
-                raise serializers.ValidationError(e.message_dict if hasattr(e, 'message_dict') else {'non_field_errors': e.messages})  # Error se atrapa dentro de DRF y view devuelve 400 con detalles
+    # sobreescritura de validate de DRF o hook/gancho que forma parte del ciclo de vida del serializer. 
+    # Permite validacion personalizada sobre objetos (varios campos a la vez)
+    # integra el clean al flujo validacion de DRF
+    def validate(self, data):   #drf devuelve un 400 Bad request mediante el def clean.
+        # Crear una instancia temporal con los datos recibidos en DRF serializer.is_valid(campo o objeto) que llama internamente a este validate.
+        instance = Matricula(**data)
+        #esta instancia no tiene .pk , django trata como nueva instancia. En clean al filtrar matricula puede fallar actualizacion. 
+        # pk es necesario para diferenciar crear de actualizar en el clean. Conservamos antes de pasar al clean. Evita FALSOS POSITIVOS. 
+        if self.instance:
+            instance.pk = self.instance.pk
+        try:
+            instance.clean()  # Ejecuta reglas de negocio definidas en el modelo
+        except DjangoValidationError as e:
+            raise serializers.ValidationError(e.message_dict if hasattr(e, 'message_dict') else {'non_field_errors': e.messages})  # Error se atrapa dentro de DRF y view devuelve 400 con detalles
 
-            return data
+        return data
       

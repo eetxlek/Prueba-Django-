f0c2a66db51dde54acd32d1a819e581416c18d98
eetxlek <121231080+eetxlek@users.noreply.github.com>
2025-09-10 18:57:54 +0200

    Update settings.py

--- Proyecto/academia_api/settings.py
+++ Proyecto/academia_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jslt7hl+6l5umi6*3o8f*fwu!-c4+c82_x=(3be+_7qp$j##h#'
 
 # SECURITY WARNING: don't run with debug turned on in production! En produccion false
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['academia-django.onrender.com']
 

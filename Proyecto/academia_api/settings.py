606e98e9965dc3115379ceb6b98c622cdc4c5bda
eetxlek <121231080+eetxlek@users.noreply.github.com>
2025-09-11 16:32:10 +0200

    Update settings.py

--- Proyecto/academia_api/settings.py
+++ Proyecto/academia_api/settings.py
@@ -53,6 +53,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← justo después de SecurityMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
